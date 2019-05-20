a821a956499ed17df1437c3811848d97e55a9bef
Mush Kabalo <mushkab@Mush-kab-mbp15.local>
2019-05-20 15:26:46 +0300

    fix schemas response

--- src/controller/schemas.ts
+++ src/controller/schemas.ts
@@ -12,35 +12,37 @@ export async function findSchemas(req, res) {
     instance,
   });
 
-  res.json([
-    {
-      id: '123',
-      ttl: 10000,
-      allowedOperations: ['find', 'count', 'get'],
-      fields: {
-        _id: {
-          displayName: '_id',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        x: {
-          displayName: 'x',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        y: {
-          displayName: 'y',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        z: {
-          displayName: 'z',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+  res.json({
+    schemas: [
+      {
+        id: '123',
+        ttl: 10000,
+        allowedOperations: ['find', 'count', 'get'],
+        fields: {
+          _id: {
+            displayName: '_id',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          x: {
+            displayName: 'x',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          y: {
+            displayName: 'y',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          z: {
+            displayName: 'z',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
         },
       },
-    },
-  ]);
+    ],
+  });
 }
 
 export async function listSchemas(req, res) {
@@ -52,33 +54,35 @@ export async function listSchemas(req, res) {
     aspects: req.aspects,
     instance,
   });
-  res.json([
-    {
-      id: '123',
-      ttl: 10000,
-      allowedOperations: ['find', 'count', 'get'],
-      fields: {
-        _id: {
-          displayName: '_id',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        x: {
-          displayName: 'x',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        y: {
-          displayName: 'y',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
-        },
-        z: {
-          displayName: 'z',
-          type: 'text',
-          queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+  res.json({
+    schemas: [
+      {
+        id: '123',
+        ttl: 10000,
+        allowedOperations: ['find', 'count', 'get'],
+        fields: {
+          _id: {
+            displayName: '_id',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          x: {
+            displayName: 'x',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          y: {
+            displayName: 'y',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
+          z: {
+            displayName: 'z',
+            type: 'text',
+            queryOperators: ['eq', 'ne', 'startsWith', 'endsWith'],
+          },
         },
       },
-    },
-  ]);
+    ],
+  });
 }
